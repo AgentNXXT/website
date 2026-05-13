f1290e1cb252ec63615cd89138a6e45d5e40a811
Chinmay Panda <chinmay@openautonomyx.com>
2026-05-13 02:37:50 -0700

    Configure static export for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/website/' : '',
   images: {
     unoptimized: true,
   },
